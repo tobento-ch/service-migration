56bc1660193af1c3dd7d3b96e14b3bb03cc3d8c4
TOBENTOch <info@tobento.ch>
2023-07-01 09:19:02 +0200

    initial commit

---
+++ src/Action/NullAction.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * TOBENTO
+ *
+ * @copyright   Tobias Strub, TOBENTO
+ * @license     MIT License, see LICENSE file distributed with this source code.
+ * @author      Tobias Strub
+ * @link        https://www.tobento.ch
+ */
+
+declare(strict_types=1);
+
+namespace Tobento\Service\Migration\Action;
+
+use Tobento\Service\Migration\ActionInterface;
+use Tobento\Service\Migration\ActionFailedException;
+
+/**
+ * NullAction
+ */
+class NullAction implements ActionInterface
+{
+    /**
+     * Create a new instance.
+     *
+     * @param null|string $name A name of the action.
+     * @param string $description A description of the action.
+     */
+    public function __construct(
+        protected null|string $name = null,
+        protected string $description = '',
+    ) {}
+        
+    /**
+     * Process the action.
+     *
+     * @return void
+     * @throws ActionFailedException
+     */
+    public function process(): void
+    {
+        //
+    }
+    
+    /**
+     * Returns a name of the action.
+     *
+     * @return string
+     */
+    public function name(): string
+    {
+        return $this->name ?: $this::class;
+    }
+
+    /**
+     * Returns a description of the action.
+     *
+     * @return string
+     */
+    public function description(): string
+    {
+        return $this->description;
+    }
+    
+    /**
+     * Returns the processed data information.
+     *
+     * @return array<array-key, string>
+     */
+    public function processedDataInfo(): array
+    {
+        return [];
+    }
+}
---
+++ tests/Action/NullActionTest.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * TOBENTO
+ *
+ * @copyright   Tobias Strub, TOBENTO
+ * @license     MIT License, see LICENSE file distributed with this source code.
+ * @author      Tobias Strub
+ * @link        https://www.tobento.ch
+ */
+
+declare(strict_types=1);
+
+namespace Tobento\Service\Migration\Test\Action;
+
+use PHPUnit\Framework\TestCase;
+use Tobento\Service\Migration\Action\NullAction;
+use Tobento\Service\Migration\ActionInterface;
+use Tobento\Service\Migration\ActionFailedException;
+
+/**
+ * NullActionTest
+ */
+class NullActionTest extends TestCase
+{
+    public function testIsInstanceofActionInterface()
+    {
+        $this->assertInstanceOf(
+            ActionInterface::class,
+            new NullAction()
+        );
+    }
+    
+    public function testProcessMethod()
+    {
+        $action = new NullAction();
+        
+        $action->process();
+
+        $this->assertTrue(true);
+    }
+
+    public function testNameMethod()
+    {
+        $this->assertSame(NullAction::class, (new NullAction())->name());
+        
+        $this->assertSame('foo', (new NullAction(name: 'foo'))->name());
+    }
+    
+    public function testDescriptionMethod()
+    {
+        $this->assertSame('', (new NullAction())->description());
+        
+        $this->assertSame('foo', (new NullAction(description: 'foo'))->description());
+    }
+    
+    public function testProcessedDataInfoMethod()
+    {
+        $action = new NullAction();
+        $action->process();
+        
+        $this->assertSame([], $action->processedDataInfo());
+    }
+}
