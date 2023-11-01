30c8b6a20408f1f247a9f95be925351289911423
TOBENTOch <info@tobento.ch>
2023-11-01 12:19:36 +0100

    initial commit

---
+++ src/Action/CallableAction.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * TOBENTO
+ *
+ * @copyright   Tobias Strub, TOBENTO
+ * @license     MIT License, see LICENSE file distributed with this source code.
+ * @author      Tobias Strub
+ * @link        https://www.tobento.ch
+ */
+
+declare(strict_types=1);
+
+namespace Tobento\Service\Migration\Action;
+
+use Tobento\Service\Migration\ActionInterface;
+use Tobento\Service\Migration\ActionFailedException;
+
+/**
+ * CallableAction
+ */
+class CallableAction implements ActionInterface
+{
+    /**
+     * @var callable
+     */
+    protected $callable;
+    
+    /**
+     * Create a new instance.
+     *
+     * @param callable $callable
+     * @param array $parameters Any parameters passed to the callable.
+     * @param null|string $name A name of the action.
+     * @param string $description A description of the action.
+     */
+    public function __construct(
+        callable $callable,
+        protected array $parameters = [],
+        protected null|string $name = null,
+        protected string $description = '',
+    ) {
+        $this->callable = $callable;
+    }
+        
+    /**
+     * Process the action.
+     *
+     * @return void
+     * @throws ActionFailedException
+     */
+    public function process(): void
+    {
+        call_user_func_array($this->getCallable(), $this->getParameters());
+    }
+    
+    /**
+     * Returns a name of the action.
+     *
+     * @return string
+     */
+    public function name(): string
+    {
+        return $this->name ?: $this::class;
+    }
+
+    /**
+     * Returns a description of the action.
+     *
+     * @return string
+     */
+    public function description(): string
+    {
+        return $this->description;
+    }
+    
+    /**
+     * Returns the processed data information.
+     *
+     * @return array<array-key, string>
+     */
+    public function processedDataInfo(): array
+    {
+        return [];
+    }
+    
+    /**
+     * Returns the callable.
+     *
+     * @return callable
+     */    
+    public function getCallable(): callable
+    {
+        return $this->callable; 
+    }
+    
+    /**
+     * Returns the parameters.
+     *
+     * @return array
+     */    
+    public function getParameters(): array
+    {
+        return $this->parameters; 
+    }
+}
---
+++ tests/Action/CallableActionTest.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * TOBENTO
+ *
+ * @copyright   Tobias Strub, TOBENTO
+ * @license     MIT License, see LICENSE file distributed with this source code.
+ * @author      Tobias Strub
+ * @link        https://www.tobento.ch
+ */
+
+declare(strict_types=1);
+
+namespace Tobento\Service\Migration\Test\Action;
+
+use PHPUnit\Framework\TestCase;
+use Tobento\Service\Migration\Action\CallableAction;
+use Tobento\Service\Migration\ActionInterface;
+use Tobento\Service\Migration\ActionFailedException;
+
+/**
+ * CallableActionTest
+ */
+class CallableActionTest extends TestCase
+{
+    public function testIsInstanceofActionInterface()
+    {
+        $this->assertInstanceOf(
+            ActionInterface::class,
+            new CallableAction(function () {})
+        );
+    }
+    
+    public function testProcessMethod()
+    {
+        $action = new CallableAction(
+            callable: function () {
+                
+            },
+            parameters: [],
+        );
+        
+        $action->process();
+        
+        $this->assertTrue(true);
+    }
+
+    public function testNameMethod()
+    {
+        $this->assertSame(CallableAction::class, (new CallableAction(function () {}))->name());
+        
+        $this->assertSame('foo', (new CallableAction(function () {}, name: 'foo'))->name());
+    }
+    
+    public function testDescriptionMethod()
+    {
+        $this->assertSame('', (new CallableAction(function () {}))->description());
+        
+        $this->assertSame('foo', (new CallableAction(function () {}, description: 'foo'))->description());
+    }
+    
+    public function testProcessedDataInfoMethod()
+    {
+        $action = new CallableAction(function () {});
+        $action->process();
+        
+        $this->assertSame([], $action->processedDataInfo());
+    }
+    
+    public function testGetCallableMethod()
+    {
+        $callable = function () {};
+        
+        $action = new CallableAction($callable);
+        
+        $this->assertSame($callable, $action->getCallable());
+    }
+    
+    public function testGetParametersMethod()
+    {
+        $parameters = ['name' => 'value'];
+        $action = new CallableAction(function () {}, $parameters);
+        
+        $this->assertSame($parameters, $action->getParameters());
+    }
+}
